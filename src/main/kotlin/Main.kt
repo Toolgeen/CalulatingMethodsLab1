583b40c884829061a0b929d30d73975cfb6de2b2
Tol <htemohpab@gmail.com>
2022-06-24 17:15:50 +0600

    fun findIntervals

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 import kotlin.math.*
 
+var count : Int = 0
+
 fun main() {
 
 }
 
 fun baseFunction(x: Double) : Double {
+    count++
     return (10 * cos(x) - 0.1 * x.pow(2))
 }
 
@@ -16,4 +19,19 @@ private fun validateInsert(insert: String?): Boolean {
             insert.trim().toDouble() >= 0
         } else false
     }
+}
+
+private fun findIntervals (function: (Double) -> Double, a: Double, b: Double, h: Double) : Array<DoubleArray> {
+    val answer = mutableListOf<DoubleArray>()
+    var rightBorder = a
+    var leftBorder = a + h
+    while (rightBorder < b) {
+        if (function(leftBorder)*function(rightBorder) <= 0) {
+            answer.add(doubleArrayOf(leftBorder,rightBorder))
+        } else {
+            leftBorder = rightBorder
+            rightBorder += h
+        }
+    }
+    return answer.toTypedArray()
 }
