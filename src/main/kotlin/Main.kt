89f63e58525f2c9714939fdeae2926343af93c2a
Tol <htemohpab@gmail.com>
2022-06-24 16:59:44 +0600

    fun validate Insert

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,3 +7,13 @@ fun main() {
 fun baseFunction(x: Double) : Double {
     return (10 * cos(x) - 0.1 * x.pow(2))
 }
+
+private fun validateInsert(insert: String?): Boolean {
+    return if (insert == null) {
+        false
+    } else {
+        if (insert.trim().toDoubleOrNull() is Double) {
+            insert.trim().toDouble() >= 0
+        } else false
+    }
+}
